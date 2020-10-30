6224ec442abda9834ea2a5bbaf8e25ef2d48718c
Zinadin Zidan <zidan44@pixelexperience.org>
2020-10-30 23:19:29 +0600

    mtk-vendor: treewide: Switch To Blueprint
    * Also clean some unused files.
    
    Co-Authored-By: Elias <eliasgheeraert@gmail.com>
    Change-Id: I297c877c7f323a2ffb2a29509b29fb8d23a971d0

--- connectivity/bluetooth/driver/mt66xx/Android.mk
+++
@@ -1,5 +0,0 @@
-ifeq ($(MTK_BT_SUPPORT), yes)
-ifeq ($(filter MTK_MT76%, $(MTK_BT_CHIP)),)
-include $(call all-subdir-makefiles)
-endif
-endif
--- connectivity/bluetooth/driver/mt76xx/Android.mk
+++
@@ -1,5 +0,0 @@
-ifeq ($(MTK_BT_SUPPORT), yes)
-ifneq ($(filter MTK_MT76%, $(MTK_BT_CHIP)),)
-include $(call all-subdir-makefiles)
-endif
-endif
